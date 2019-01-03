c10f7fb97c53eb8b429a4919e619556aa3ba04d9
anilg-clarion <anil.gudur@clariontechnologies.co.in>
2019-01-03 22:41:42 +0530

    Section 3.47 - Working with inline styles.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import "./Person.css";
+
 const person = props => {
   // I'm a {props.name} and I am {Math.floor(Math.random() * 30)} years old!
   return (
-    <div>
+    <div class="Person">
       <p onClick={props.click}>
         I'm a {props.name} and I am {props.age} years old!
       </p>
