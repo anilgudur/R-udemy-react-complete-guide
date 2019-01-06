69253fb176f48bf19c9a8d197769aa2d75c8e274
anilgudur <anilg.cse@gmail.com>
2019-01-06 22:40:57 +0530

    Section 7.81 - Splitting an App into components.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import classes from "./App.module.css";
-//import Person from "../components/Persons/Person/Person";
 import Persons from "../components/Persons/Persons";
 import CockPit from "../components/Cockpit/Cockpit";
 
@@ -14,31 +13,9 @@ class App extends Component {
     showPersons: false
   };
 
-  // nameHandler = newName => {
-  //   //console.log("Was clicked");
-  //   this.setState({
-  //     persons: [
-  //       { name: newName, age: 28 },
-  //       { name: "Manu", age: 29 },
-  //       { name: "Stephanie", age: 27 }
-  //     ]
-  //   });
-  // };
-
   nameChangedHandler = (id, event) => {
-    // this.setState({
-    //   persons: [
-    //     { name: "Max", age: 28 },
-    //     { name: event.target.value, age: 29 },
-    //     { name: "Stephanie", age: 27 }
-    //   ]
-    // });
-    // console.log("event", event);
-    // console.log("event.target.value", event.target.value);
-    // console.log("id", id);
     const personIndex = this.state.persons.findIndex(p => p.id === id);
     const person = { ...this.state.persons[personIndex] };
-    // const person = Object.assign({}, this.state.persons[personIndex]);
     person.name = event.target.value;
     const persons = [...this.state.persons];
     persons[personIndex] = person;
@@ -50,7 +27,6 @@ class App extends Component {
   };
 
   deletePersonHandler = index => {
-    //const persons = this.state.persons.slice(); // or
     const persons = [...this.state.persons];
     persons.splice(index, 1);
     this.setState({
@@ -59,14 +35,6 @@ class App extends Component {
   };
 
   render() {
-    //   <Person
-    //   name={this.state.persons[1].name}
-    //   age={this.state.persons[1].age}
-    //   click={this.nameHandler.bind(this, "Max!")}
-    //   nameChangeClick={this.nameChangedHandler}
-    // >
-    //   My Hobbies: Racing
-    // </Person>
     let persons = null;
 
     if (this.state.showPersons) {
@@ -78,17 +46,6 @@ class App extends Component {
         />
       );
     }
-    // this.state.persons.map((person, index) => {
-    //   return (
-    //     <Person
-    //       name={person.name}
-    //       age={person.age}
-    //       deletePersonClick={this.deletePersonHandler.bind(this, index)}
-    //       nameChangeClick={this.nameChangedHandler.bind(this, person.id)}
-    //       key={person.id}
-    //     />
-    //   );
-    // })
 
     return (
       <div className={classes.App}>
