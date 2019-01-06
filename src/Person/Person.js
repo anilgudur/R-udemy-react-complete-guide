fa7de78a285b118b9f2d369f5a67799dac8a9325
anilgudur <anilg.cse@gmail.com>
2019-01-06 18:36:22 +0530

    Section 5.67 - CSS Modules used.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,16 +1,16 @@
 import React from "react";
-import "./Person.css";
+import classes from "./Person.module.css";
 
 const person = props => {
   // I'm a {props.name} and I am {Math.floor(Math.random() * 30)} years old!
   // <p onClick={props.click}>
   return (
-    <div className="Person">
+    <div className={classes.Person}>
       <p onClick={props.deletePersonClick}>
         I'm a {props.name} and I am {props.age} years old!
       </p>
       <p>{props.children}</p>
-      <input type="text" onChange={props.nameChangeClick} value={props.name} />
+      <input type='text' onChange={props.nameChangeClick} value={props.name} />
     </div>
   );
 };
